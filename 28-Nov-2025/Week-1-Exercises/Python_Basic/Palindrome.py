6c16ffe0ec551181f1405bac9e6f1d2020f4ad27
Aratrika11 <95957314+Aratrika11@users.noreply.github.com>
2025-11-28 09:11:43 +0000

    change

--- 28-Nov-2025/Week-1-Exercises/Python_Basic/Palindrome.py
+++
@@ -1,5 +0,0 @@
-str=input("Enter a string: ")
-if str == str[::-1]:
-    print (str," is a palindrome")
-else:
-    print (str," is not a palindrome")
--- 28-Nov-2025/Week-1-Exercises/Python_Basic/Remove_SplChr.py
+++
@@ -1,6 +0,0 @@
-str=input("Enter a string")
-rstr=""
-for i in str:
-    if i.isalnum():
-        rstr+=i
-print("String without special characters: ",rstr)
