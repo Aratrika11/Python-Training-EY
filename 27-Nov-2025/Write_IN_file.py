428b67aaf349a0f3e76567fbf894c946dcb62899
Aratrika11 <95957314+Aratrika11@users.noreply.github.com>
2025-11-30 16:24:22 +0530

    Delete 27-Nov-2025/Write_IN_file.py

--- 27-Nov-2025/Write_IN_file.py
+++
@@ -1,6 +0,0 @@
-with open("Notes.txt","w")as f:
-    f.write("Hello World")
-    f.write("\nHow is the day \n")
-    f.write("My name is Aratrika Debnath\n")
-    f.write("I am an ASE in EY GDS\n")
-    f.write("This is my first job")
